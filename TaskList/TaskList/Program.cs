126f650aa9e7b9b319e6a23a49d93ff4697936e7
Milena <mp.pokorska@gmail.com>
2017-10-17 13:54:06 +0200

    Szkielet aplikacji konsolowej.

--- TaskList/TaskList/Program.cs
+++ TaskList/TaskList/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Text;
+using System.Collections.Generic;
 
 namespace TaskList
 {
@@ -11,12 +12,15 @@ namespace TaskList
             Console.InputEncoding = Encoding.GetEncoding(852);
             Console.OutputEncoding = Encoding.UTF8;
 
+            List < Task > lista = new List<Task>();
+
+
             do
             {
                 Console.WriteLine("Wpisz polecenie:");
                 string command = Console.ReadLine();
                 if (command=="exit") { break; }
-
+                
 
             } while (true);
 
